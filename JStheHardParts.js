af704a96ca1e493ecfa185a7edabe6a835b80155
orjames <orjames@berkeley.edu>
2019-06-13 11:30:22 -0700

    adding

--- JStheHardParts.js
+++ JStheHardParts.js
@@ -1,3 +1,5 @@
+// Objects - store functions with their associated data
+
 const user1 = {
   name: 'Phil',
   score: 4,
@@ -7,5 +9,5 @@ const user1 = {
 };
 
 user1.increment(); //user1.score => 4
-
+console.log(user1.score);
 // this is the principle of encapsulation
